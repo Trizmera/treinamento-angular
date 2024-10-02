69bc9a0562553b7d70cabb37c9e0b52d1cda72bc
Beatriz Cruz <mllebia@live.com>
2024-10-02 14:15:21 -0300

    adiciona card teste

--- lesson-2/src/app/housing-location/housing-location.component.ts
+++ lesson-2/src/app/housing-location/housing-location.component.ts
@@ -7,9 +7,7 @@ import { HousingLocation } from '../housinglocation';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+   
   `,
   styleUrls: ['./housing-location.component.css']
 })
