d5220a5a1f18883cab4140aced05d108a5369405
Beatriz Cruz <mllebia@live.com>
2024-10-04 11:12:36 -0300

    versao final utilizando json

--- lesson-11/src/app/home/home.component.ts
+++ lesson-11/src/app/home/home.component.ts
@@ -41,8 +41,10 @@ export class HomeComponent {
   }
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.filteredLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 
 }
